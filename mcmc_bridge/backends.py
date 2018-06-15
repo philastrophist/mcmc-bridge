e5e0f02bcca2f35104157f93cac78b8e21bdff82
Shaun C Read <shaun.c.read@gmail.com>
2018-06-15 15:02:05 +0100

    incomplete; failing tests

--- mcmc_bridge/backends.py
+++ mcmc_bridge/backends.py
@@ -1,3 +1,4 @@
+import emcee
 import numpy as np
 from pymc3.backends import NDArray
 from pymc3.backends.base import MultiTrace
@@ -7,41 +8,43 @@ __all__ = ['EmceeTrace']
 class Indexer(object):
     def __init__(self, walker_trace):
         self.walker_trace = walker_trace
-
+        self.emcee_backend = self.walker_trace.emcee_backend
         self.chain = self.walker_trace.chain
-        self.raw_samples = self.walker_trace.emcee_trace_obj.sampler.chain
-        self.raw_blobs = self.walker_trace.emcee_trace_obj.sampler.blobs
-        self.unfitted_chain = self.walker_trace.emcee_trace_obj.unfitted_chain
-        self.ordering = self.walker_trace.emcee_trace_obj.sampler.ordering
+
+
+        # self.raw_samples = self.walker_trace.emcee_backend.sampler.chain
+        # self.raw_blobs = self.walker_trace.emcee_backend.sampler.blobs
+        self.unfitted_chain = self.walker_trace.emcee_backend.unfitted_chain
+        self.ordering = self.walker_trace.emcee_backend.ordering
 
     def __getitem__(self, varname):
         try:
             varmap = self.ordering[varname]
-            samples = self.raw_samples[self.chain, :, varmap.slc]
+            samples = self.emcee_backend[self.chain, :, varmap.slc]
             return samples.reshape(samples.shape[:1]+varmap.shp).astype(varmap.dtyp)
         except KeyError:
             dslice, dshape = self.unfitted_chain[varname]
             return self.raw_blobs[:, self.chain, dslice].reshape(dshape)
 
 
-    def items(self):
-        names = [i.name for i in self.walker_trace.model.vars] + self.walker_trace.emcee_trace_obj.sampler.unobserved_varnames
-        return zip(names, [self[n] for n in names])
+    # def items(self):
+    #     names = self.walker_trace.fitted_varnames + self.walker_trace.unobserved_varnames
+    #     return zip(names, [self[n] for n in names])
 
 
 
 class EmceeWalkerTrace(NDArray):
-    def __init__(self, emcee_trace_obj, chain, name=None):
-        super(NDArray, self).__init__(name, model=emcee_trace_obj.sampler.model)
-        self.emcee_trace_obj = emcee_trace_obj
+    def __init__(self, emcee_backend, chain, name=None):
+        super(NDArray, self).__init__(name, model=emcee_backend.sampler.model)
+        self.emcee_trace_obj = emcee_backend
         self.chain = chain
-        self.draw_idx = emcee_trace_obj.sampler.chain.shape[1]
+        self.draw_idx = emcee_backend.iteration
         self.draws = self.draw_idx
         self._stats = None
 
     @property
     def fitted_varnames(self):
-        return [i.name for i in self.emcee_trace_obj.sampler.model.vars]
+        return self.emcee_trace_obj.varnames
 
     @property
     def samples(self):
@@ -71,8 +74,10 @@ def unpack_param_blobs(sampler):
 
 
 class EmceeTrace(MultiTrace):
-    def __init__(self, sampler):
-        self.sampler = sampler
-        self.unfitted_chain = unpack_param_blobs(sampler)
-        traces = [EmceeWalkerTrace(self, i) for i in range(sampler.chain.shape[0])]
+    def __init__(self, backend):
+        if isinstance(backend, emcee.EnsembleSampler):
+            backend = backend.backend
+        self.backend = backend
+        self.unfitted_chain = unpack_param_blobs(backend)
+        traces = [EmceeWalkerTrace(self, i) for i in range(backend.nwalkers)]
         super(EmceeTrace, self).__init__(traces)
